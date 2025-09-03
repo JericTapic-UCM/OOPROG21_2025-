130d752f4722ad806ddb51e5f2ac115ca696126a
JericTapic-UCM <jerictapic8@gmail.com>
2025-09-03 20:06:41 +0800

    Create Comments.java

---
+++ Chapter _1/Comments.java
@@ -0,0 +1,17 @@
+public class Comments{
+public static void main(String [] args){
+
+// single line comment
+/* multi-line Comment*/
+/**
+* This is a documentation comment
+*/
+
+
+
+
+
+     }
+
+
+   }
