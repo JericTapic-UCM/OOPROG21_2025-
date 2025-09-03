b0d8402f8cc6447838df450283377af2815bf583
JericTapic-UCM <jerictapic8@gmail.com>
2025-09-03 08:42:26 +0800

    Create FavoriteSong.java

---
+++ Chapter _1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong{
+public static void main(String [] args){
+
+       System.out.println("Wiseman closed his mouth \nMadman closed his fist\n Young man shows his age  \nJudge man named it sin");
+
+
+
+     }
+  }
