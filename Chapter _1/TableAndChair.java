48d09bd436bfb40884f0f5205e651eb625cf7668
JericTapic-UCM <jerictapic8@gmail.com>
2025-09-03 08:40:22 +0800

    Create TableAndChair.java

---
+++ Chapter _1/TableAndChair.java
@@ -0,0 +1,19 @@
+public class TableandChair{
+public static void main(String [] args){
+
+   for(int i=1;i<=8;i++){
+System.out.println("*                                                   *");
+  }
+
+for(int i=0;i<1;i++){
+System.out.println("*************         ***************   *************");
+
+  }
+
+for(int i = 1;i < 8;i++){
+System.out.println("*           *                *           *          *");
+
+}
+}
+
+}
