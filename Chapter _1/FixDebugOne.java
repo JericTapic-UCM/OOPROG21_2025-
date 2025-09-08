6a1c9e1db03645b07ad9ced9d4a7eebf9aa53af4
JericTapic-UCM <jerictapic8@gmail.com>
2025-09-08 18:05:22 +0800

    Create FixDebugOne.java

---
+++ Chapter _1/FixDebugOne.java
@@ -0,0 +1,24 @@
+public class FixDebugOne
+{
+/* This program displays a greeting */
+public static void main(String []args)
+{
+System.out.println("Hello World!");
+}
+}
+/*
+List down the spotted bugs below:
+
+wrong syntax "Main" instead of "main"
+
+wrong syntax for access modifier "(String  args)" instead of "(String [] args)"
+
+missing "}"
+wrong syntax "Systm.out.println("Hello World!")"  instead of "System.out.println("Hello World!");
+    
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. (input enteries here...)
+2. ....
+*/
