5bc6de91659c75970ac15ad481f2e396c1434ecc
JericTapic-UCM <jerictapic8@gmail.com>
2025-11-18 19:44:09 +0800

    Create CountGreaterThanAverage.java

---
+++ Chapter _7/CountGreaterThanAverage.java
@@ -0,0 +1,26 @@
+public class CountGreaterThanAverage {
+
+    public static int countGreater(int[] arr) {
+        if (arr.length <= 1) return 0;
+
+        int count = 0;
+        double sum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            double avg = sum / i;
+
+            if (arr[i] > avg) {
+                count++;
+            }
+
+            sum += arr[i];
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] responseTimes = {100, 200, 150, 300};
+        System.out.println(countGreater(responseTimes));
+    }
+}
